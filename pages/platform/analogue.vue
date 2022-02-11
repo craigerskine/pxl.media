b5c8cfe9715c9389ecbfa113ddb987b82f48af72
Craig - qrayg <craig@craigerskine.com>
2022-02-11 16:06:05 -0600

    missing pages

---
+++ pages/platform/analogue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <mast
+      title="Analogue"
+      logo="item.logo"
+      data_1="0"
+      data_1_label="Games"
+      data_2="##"
+      data_2_label="Systems"
+    />
+    <heading>Games owned</heading>
+    <p>Coming soon...</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: 'Analogue',
+      };
+    },
+  }
+</script>
---
+++ pages/platform/mini.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <mast
+      title="Mini"
+      logo="item.logo"
+      data_1="0"
+      data_1_label="Games"
+      data_2="##"
+      data_2_label="Systems"
+    />
+    <heading>Games owned</heading>
+    <p>Coming soon...</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: 'Mini',
+      };
+    },
+  }
+</script>
