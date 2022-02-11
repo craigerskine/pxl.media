189d04c3d9fd325e208ced7e1b5786439481e319
Craig - qrayg <craig@craigerskine.com>
2022-02-11 08:37:11 -0600

    images + error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
   <h1 class="m-auto py-8 px-4 text-gray-500 text-8xl font-bold text-center">
     <template v-if="error.statusCode === 404">404</template>
-    <template v-else>An error occurred</template>
+    <template v-else>oops</template>
     <small class="text-2xl block opacity-90">↑ ↑ ↓ ↓ ← → ← → B A</small>
   </h1>
 </template>
