eb9be574298bed3caf0e79d40c123006ad52b1a5
Craig - qrayg <craig@craigerskine.com>
2022-02-16 11:34:04 -0600

    transitions, better search clear

--- static/assets/js/twind.config.js
+++ static/assets/js/twind.config.js
@@ -38,13 +38,16 @@ twind.setup({
   },
   plugins: {
     'bg-grid': { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 4 4\' width=\'4\' height=\'4\'><rect x=\'0\' y=\'0\' width=\'2\' height=\'2\' fill=\'rgba(5,5,5,.33)\'></rect></svg>")', },
-    'bg-cross': { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 4 4\' width=\'4\' height=\'4\'><g fill=\'rgba(5,5,5,.33)\' fill-rule=\'evenodd\'><path d=\'m0 0h1v1h-1z\'/><path d=\'m1 1h1v1h-1z\'/><path d=\'m2 2h1v1h-1z\'/><path d=\'m3 3h1v1h-1z\'/><path d=\'m3 1h1v1h-1z\'/><path d=\'m1 3h1v1h-1z\'/></g></svg>")', },
+    'bg-close': { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 10 10\' width=\'10\' height=\'10\'><g fill=\'rgba(255,255,255,.66)\' fill-rule=\'evenodd\'><polygon points=\'8 0 10 2 7 5 10 8 8 10 5 7 2 10 0 8 3 5 0 2 2 0 5 3\'></polygon></g></svg>")', },
   },
 })
 twind.tw(() => ({
   '@global': {
     '.input-search:hover + svg': { '@apply': 'opacity-100', },
+    '.input-search::-webkit-search-cancel-button': { '@apply': 'cursor-pointer appearance-none w-3 h-3 bg(close no-repeat center)', },
     '.box': { 'background-color': 'rgba(255,255,255,.05)', 'box-shadow': 'inset 0 1px 0 0 rgba(255,255,255,.075)', },
+    '.page-enter-active,.page-leave-active': { '@apply': 'transition', },
+    '.page-enter,.page-leave-to': { '@apply': 'opacity-0', },
   }
 }))
 twindObserve.observe(document.documentElement);
