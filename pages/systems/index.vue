4bcc044d6e48aab6851d7475a89aa9ea8c416dd0
Craig - qrayg <craig@craigerskine.com>
2022-02-18 11:18:15 -0600

    systems link to platforms

--- pages/systems/index.vue
+++ pages/systems/index.vue
@@ -3,12 +3,12 @@
     <heading>Systems</heading>
     <ul class="container mx-auto mb-12 px-4 md:(grid(& flow-row cols-2) gap-12)">
       <li v-for="platform of systemPlatform" class="mb-12 md:(mb-0)">
-        <p class="py-5 border(b dashed gray-500 opacity-30)">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 60" role="img" class="w-auto h-12 max-w-full fill-current opacity-50">
+        <nuxt-link :to="'/platform/'+ platform.slug +'/'" class="group py-5 border(b dashed gray-500 opacity-30) block transition hover:(text-white) focus:(text-white)">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 60" role="img" class="w-auto h-12 max-w-full fill-current opacity-50 transition group-hover:(opacity-100) group-focus:(opacity-100)">
             <title>{{ platform.title }}</title>
             <g v-html="platform.logo"></g>
           </svg>
-        </p>
+        </nuxt-link>
         <ul class="mt-5 ml-5 pl-5 border(l dashed gray-500 opacity-30) divide(y dashed gray-500 opacity-30) md:(ml-12 pl-12)">
           <template v-if="systemIndexFilter(platform.slug).length">
             <li v-for="system of systemIndexFilter(platform.slug)" class="py-2 flex(& wrap) items-center">
