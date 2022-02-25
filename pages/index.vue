5be849b1e53aafb804a8696a40e5ad79b8fedff8
Craig - qrayg <craig@craigerskine.com>
2022-02-25 11:15:58 -0600

    ...

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul class="-ml-[50vw] -mr-[50vw] w-screen justify-around relative left-1/2 right-1/2 lg:(flex space-x-4)">
+    <ul class="-ml-[50vw] -mr-[50vw] w-screen justify-center relative left-1/2 right-1/2 lg:(flex space-x-4)">
       <li class="stat mx-auto px-4 py-1 border-b border-gray-500 border-opacity-30 w-full sm:(max-w-md) lg:(mx-0 my-10)">
         <mast
           label="Games"
