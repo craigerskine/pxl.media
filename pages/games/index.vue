1d0bac9acf898fc2a3ec692d8b32e0dab308327f
Craig - qrayg <craig@craigerskine.com>
2022-02-26 15:28:43 -0600

    ...

--- pages/games/index.vue
+++ pages/games/index.vue
@@ -1,25 +1,48 @@
 <template>
   <div>
-    <heading>Games</heading>
-    <ul class="m-0 grid(& flow-row cols-3) gap-10">
-      <li v-for="game in gamesAll">
-        <figure class="w-full max-w-screen-sm h-10 bg-yellow-400">
-          <img :src="'/assets/img/games/'+ game.slug +'.jpg'" class="w-full h-10 object-cover filter grayscale" />
-        </figure>
-        <small v-text="game.slug" class="text-xs font-mono"></small>
-      </li>
+    <heading :subtext="gamesAllTotal">Games</heading>
+    <ul class="list-game pb-4 flex flex-wrap">
+      <game v-for="game of gamesAllPaged"
+        :key="game.slug"
+        :title="game.title"
+        :slug="game.slug"
+        :note="game.note"
+        :platform="game.platform"
+        :genre="game.genre"
+        :physical="game.physical"
+        :digital="game.digital"
+        :guide="game.guide"
+        :pending="game.pending"
+        :posted="game.posted"
+      />
     </ul>
+    <pagination v-if="gamesAllTotal > 24" pagePath="/games" :pageTotal="gamesAllTotal" />
   </div>
 </template>
 
 <script>
   export default {
-    async asyncData({ $content }) {
-      const gamesAll = await $content("games")
-        .sortBy('title', 'asc')
+    async asyncData({ $content, params }) {
+      const gamesAllPageCurrent = parseInt(params.page);
+      const gamesAllPagePer = 24;
+      const gamesAll = await $content("games").only(['title']).fetch();
+      const gamesAllTotal = gamesAll.length;
+      const gamesAllLastPage = Math.ceil(gamesAllTotal / gamesAllPagePer);
+      const gamesAllLastPageCount = gamesAllTotal % gamesAllPagePer === 0 ? gamesAllPagePer : gamesAllTotal % gamesAllPagePer;
+      const gamesAllSkip = () => {
+        if (gamesAllPageCurrent === 1) {return 0;}
+        if (gamesAllPageCurrent === gamesAllLastPage) {return gamesAllTotal - gamesAllLastPageCount;}
+        return (gamesAllPageCurrent - 1) * gamesAllPagePer;
+      };
+      const gamesAllPaged = await $content("games")
+        .sortBy('title')
+        .limit(gamesAllPagePer)
+        .skip(gamesAllSkip())
         .fetch();
       return {
         gamesAll,
+        gamesAllTotal,
+        gamesAllPaged
       };
     },
     head() {
--- pages/games/pending.vue
+++ pages/games/pending.vue
@@ -1,23 +1,21 @@
 <template>
   <div>
     <heading :subtext="gamesPending.length">Pending</heading>
-    <section class="container mx-auto flex flex-wrap">
-      <ul class="list-game pb-4 flex flex-wrap">
-        <game v-for="game of gamesPending"
-          :key="game.slug"
-          :title="game.title"
-          :slug="game.slug"
-          :note="game.note"
-          :platform="game.platform"
-          :genre="game.genre"
-          :physical="game.physical"
-          :digital="game.digital"
-          :guide="game.guide"
-          :pending="game.pending"
-          :posted="game.posted"
-        />
-      </ul>
-    </section>
+    <ul class="list-game pb-4 flex flex-wrap">
+      <game v-for="game of gamesPending"
+        :key="game.slug"
+        :title="game.title"
+        :slug="game.slug"
+        :note="game.note"
+        :platform="game.platform"
+        :genre="game.genre"
+        :physical="game.physical"
+        :digital="game.digital"
+        :guide="game.guide"
+        :pending="game.pending"
+        :posted="game.posted"
+      />
+    </ul>
   </div>
 </template>
 
