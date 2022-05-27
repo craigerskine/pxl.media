48ed735c2c5fc87e2ec521a2050cf2125df58c35
Craig - qrayg <craig@craigerskine.com>
2022-05-27 13:12:27 -0500

    ...

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,8 @@
 <template>
-  <h1 class="m-auto py-8 px-4 text-gray-500 text-8xl font-bold text-center fixed top-1/2 left-1/2 transform -translate-1/2">
+  <h1 class="m-auto px-4 text-gray-500 text-[calc(4vw+4vh)] leading-none font-bold text-center fixed top-1/2 left-1/2 transform -translate-1/2">
     <template v-if="error.statusCode === 404">404</template>
     <template v-else>oops</template>
-    <small class="text-2xl block opacity-90">↑ ↑ ↓ ↓ ← → ← → B A</small>
+    <small class="text-lg block opacity-80 md:(text-2xl)">↑ ↑ ↓ ↓ ← → ← → B A</small>
   </h1>
 </template>
 
