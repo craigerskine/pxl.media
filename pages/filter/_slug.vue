a0965a3655e8fbbd8ef77655f412e0496fdf9f5b
Craig - qrayg <craig@craigerskine.com>
2022-08-18 14:38:44 -0500

    ...

---
+++ pages/filter/_slug.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <heading>Filter: <b v-text="this.$route.params.slug.replace('-', ' ')"></b> (<span v-text="filteredGames.length"></span>)</heading>
+    <ul class="list-game flex(& wrap)">
+      <game v-for="(filter, index) in filteredGames"
+        :key="filter.slug"
+        :title="filter.title"
+        :slug="filter.slug"
+        :note="filter.note"
+        :platform="filter.platform"
+        :genre="filter.genre"
+        :physical="filter.physical"
+        :digital="filter.digital"
+        :guide="filter.guide"
+        :pending="filter.pending"
+        :posted="filter.posted"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const filteredGames = await $content("games")
+        .sortBy('title', 'asc')
+        .search(params.slug.replace('-', ' '))
+        .fetch();
+      return {
+        filteredGames
+      };
+    },
+    // watch: {
+    //   async query(filterQuery) {
+    //     if(!filterQuery) {
+    //       this.filterResults = []
+    //       return
+    //     }
+    //     this.filterResults = await this.$content('games')
+    //       .sortBy('title', 'asc')
+    //       .search(filterQuery)
+    //       .limit(96)
+    //       .fetch();
+    //   }
+    // },
+    head() {
+      return {
+        title: 'Search',
+      };
+    },
+  }
+</script>
