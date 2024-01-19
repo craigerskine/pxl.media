131017c2092ba5d24f69ebefba5984f74bece016
Craig - qrayg <craig@craigerskine.com>
2024-01-19 13:57:26 -0600

    convert footer-link from twind class to nunjucks macro

--- _site/_app/_app.js
+++ _site/_app/_app.js
@@ -83,7 +83,6 @@ injectGlobal`
     .box { @apply bg-white/5 rounded-lg ring-(inset 1 white/5) shadow-lg; }
     .box .box-cover { @apply rounded-t-lg; }
     .box .box-cover-img { @apply ring-(inset 1 white/10); }
-    .footer-link { @apply text-brand-400 font-bold motion-safe:(transition) hover:(text-brand-500 underline); }
   }
 `
 
