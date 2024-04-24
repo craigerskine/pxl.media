39ec28069f1cb79d08e7b6c7f6b399721b466414
Craig @craigerskine @qrayg <craig@craigerskine.com>
2024-04-24 10:45:25 -0500

    tooltip to tippy, massive cleanup

--- _site/_app/_app.js
+++ _site/_app/_app.js
@@ -4,38 +4,6 @@ import 'instant.page';
 // icons
 import 'iconify-icon';
 
-// // highway - animations
-// import Highway from '@dogstudio/highway';
-// import PageTrans from './transition';
-// const H = new Highway.Core({
-//   transitions: {
-//     default: PageTrans
-//   }
-// });
-// const links = document.querySelectorAll('header .nav-item a');
-// H.on('NAVIGATE_IN', ({ to, location }) => {
-//   for (let i = 0; i < links.length; i++) {
-//     const link = links[i];
-//     link.classList.remove('active');
-//     link.removeAttribute('aria-current');
-//     if (location.href.includes(link.href)) {
-//       link.classList.add('active');
-//       link.setAttribute('aria-current', 'page');
-//     }
-//   }
-// });
-// // pageloading
-// const pageloading = document.querySelector('.loading');
-// const pageloadingprogress = document.querySelector('.loading-progress');
-// H.on('NAVIGATE_OUT', () => {
-//   pageloading.classList.remove(['opacity-0']);
-//   pageloadingprogress.classList.add(['motion-safe:animate-progress']);
-// });
-// H.on('NAVIGATE_END', () => {
-//   pageloading.classList.add(['opacity-0']);
-//   pageloadingprogress.classList.remove(['motion-safe:animate-progress']);
-// });
-
 // twind
 import { install, injectGlobal } from '@twind/core';
 import presetAutoprefix from '@twind/preset-autoprefix';
@@ -77,6 +45,7 @@ install({
     [ 'text-wrap-(unset|wrap|nowrap|balance)', 'textWrap' ],
     [ 'bg-grid', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'4\' height=\'4\' viewBox=\'0 0 4 4\'><g fill=\'rgb(128 128 128 / .05)\'><polygon points=\'4 0 4 4 3 4 3 1 0 1 0 0\' /></g></svg>")', } ],
     [ 'bg-line', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'4\' height=\'4\' viewBox=\'0 0 4 4\' fill=\'rgb(51 51 51 / .66)\'><rect x=\'0\' y=\'1\' width=\'4\' height=\'1\' /><rect x=\'0\' y=\'3\' width=\'4\' height=\'1\' /></svg>")', } ],
+    [ 'bg-close', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' width=\'1em\' height=\'1em\' fill=\'rgb(255 255 255)\'><g opacity=\'.5\'><path d=\'M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z\' /></g></svg>")', }],
   ],
 });
 injectGlobal`
@@ -84,12 +53,40 @@ injectGlobal`
     .box { @apply bg-white/5 rounded-lg ring-(inset 1 white/5) shadow-lg; }
     .box .box-cover { @apply rounded-t-lg; }
     .box .box-cover-img { @apply ring-(inset 1 white/10); }
+    .tippy-box[data-state="hidden"] { @apply opacity-0 translate-y-2; }
+    [data-tippy-root] { @apply max-w-[calc(100vw-10px)]; }
+    .tippy-box { @apply bg-black text-(gray-400 xs) font-semibold relative outline-0 opacity-100 rounded translate-y-0 motion-safe:(transition); }
+    .tippy-box[data-placement^="top"] > .tippy-arrow { @apply bottom-0 before:(bottom-[-7px] left-0 border-(t-[8px] r-[8px] b-0 l-[8px] t-[initial])) origin-top; }
+    .tippy-box[data-placement^="bottom"] > .tippy-arrow { @apply top-0 before:(top-[-7px] left-0 border-(t-0 r-[8px] b-[8px] l-[8px] b-[initial])) origin-bottom; }
+    .tippy-box[data-placement^="left"] > .tippy-arrow {@apply right-0 before:(right-[-7px] border-(t-[8px] r-0 b-[8px] l-[8px] l-[initial])) origin-left; }
+    .tippy-box[data-placement^="right"] > .tippy-arrow { @apply left-0 before:(left-[-7px] border-(t-[8px] r-[8px] b-[8px] l-0 r-[initial]) origin-right); }
+    .tippy-arrow { @apply w-4 h-4 text-black absolute before:(content-[''] absolute border-(transparent solid)); }
+    .tippy-content { @apply py-1 px-2 relative z-[1]; }
   }
 `
 
 // alpine
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import tippy from 'tippy.js';
+
+document.addEventListener('alpine:init', () => {
+  // tooltip
+  // magic: $tooltip
+  Alpine.magic('tooltip', el => message => {
+    let instance = tippy(el, { content: message, trigger: 'manual' })
+    instance.show()
+    setTimeout(() => {
+      instance.hide()
+      setTimeout(() => instance.destroy(), 150)
+    }, 2000)
+  });
+  // directive: x-tooltip
+  Alpine.directive('tooltip', (el, { expression }) => {
+    tippy(el, { content: expression })
+  });
+});
+
 Alpine.plugin(focus);
 window.Alpine = Alpine;
 Alpine.start();
