5d41623450805405aa3fc2140cc6bc44669306e1
Craig @craigerskine @qrayg <craig@craigerskine.com>
2025-11-21 09:30:29 -0600

    input close gadget

--- _site/_app/_app.js
+++ _site/_app/_app.js
@@ -48,6 +48,7 @@ install({
     ['text-wrap-(unset|wrap|nowrap|balance)', 'textWrap'],
     ['bg-gradient-radial', 'bg-[radial-gradient(circle_at_center,var(--tw-gradient-stops))]'],
     ['bg-gradient-conic', 'bg-[conic-gradient(from_180deg,var(--tw-gradient-stops))]'],
+    ['bg-close', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' width=\'1em\' height=\'1em\' fill=\'rgba(128,128,128,1)\'><g><path d=\'M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z\' /></g></svg>")', }],
     ['container-', ({ $$ }) => `mx-auto w-full max-w-${$$}`],
   ],
 });
